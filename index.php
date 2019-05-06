7fbfda564ccfb20b86e579940a07ef9b8af71165
Kat Truitt <ktruitt@mail.greenriver.edu>
2019-05-05 20:16:30 -0400

    Added Index Page Added index.php to get data, add data to the hive

---
+++ index.php
@@ -0,0 +1,133 @@
+<?php
+session_start();
+/*
+Kat Truitt
+IT328 Assignment Dating
+April 19,2019
+index.php-> renders the routes functions, sessions and etc. for the dating websites
+*/
+
+//turn on error reporting
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//Require autoload file
+require_once('vendor/autoload.php');
+require_once('model/validation.php');
+
+//create an instance of the Base class
+$f3 = Base::instance();
+$f3->config('config.ini');
+
+//Define a default root
+$f3->route('GET /', function()
+{
+    $view = new Template();
+    echo $view->render('views/home.html');
+});
+
+$f3->route('GET /home', function()
+{
+    $view = new Template();
+    echo $view->render('views/home.html');
+
+});
+
+$f3->route('GET|POST /personal', function($f3)
+{
+    //if post array is not empty
+    if(!empty($_POST))
+    {
+        //get data from form
+        $fn= $_POST['fname'];
+        $ln= $_POST['lname'];
+        $gender = $_POST['gender'];
+        $age = $_POST['age'];
+        $tel= $_POST['phone'];
+
+        //add data to hive
+        $f3->set('fname',$fn);
+        $f3->set('lname',$ln);
+        $f3->set('gender',$gender);
+        $f3->set('age',$age);
+        $f3->set('phone',$tel);
+
+        //If data is valid
+
+        if (validForm1()) {
+            $_SESSION['fname']= $fn;
+            $_SESSION['lname']= $ln;
+            $_SESSION['age']= $age;
+            $_SESSION['phone'] = $tel;
+            if (empty($gender)) {
+                $_SESSION['gender'] = "Please select a gender";
+            }
+            else {
+                $_SESSION['gender'] = $gender;
+            }
+            $f3->reroute('/profile');
+        }
+    }
+
+    $view = new Template();
+    echo $view->render('views/form1.html');
+});
+
+$f3->route('GET|POST /profile', function($f3)
+{
+    //get data from form
+    if(!empty($_POST)){
+        $email = $_POST['email'];
+        $state = $_POST['state'];
+        $seeking = $_POST['seeking'];
+        $bio = $_POST['bio'];
+
+        //add data to hive
+        $f3->set('email',$email);
+        $f3->set('state',$state);
+        $f3->set('seeking',$seeking);
+        $f3->set('bio',$bio);
+
+        //If data is valid
+        if (validForm()) {
+            $_SESSION['email']= $email;
+            $_SESSION['state']= $state;
+            $_SESSION['seeking']= $seeking;
+            $_SESSION['age']= $bio;
+            $f3->reroute('/interests');
+        }
+    }
+    $view = new Template();
+    echo $view->render('views/form2.html');
+
+});
+
+$f3->route('GET|POST /interests',function($f3)
+{
+    if(!empty($_POST))
+    {
+        //get the data
+        $interest = $_POST['interests'];
+
+        //set the data
+        $f3->set('interests', $interest);
+
+        if(validForm())
+        {
+            $_SESSION['interests'] = $interest;
+            implode(", " , $interest);
+        }
+    }
+    $view = new Template();
+    echo $view->render('views/summary.html');
+});
+
+$f3->route('GET /confirmation', function()
+{
+
+    $view = new Template();
+    echo $view->render('views/summary.html');
+
+});
+//Run Fat-free
+$f3->run();
